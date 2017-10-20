c25bbd4a0de447b1b2b14af0cdc292202dd8877a
Benjamin Blott <benjaminblott@verticalonellc.com>
2017-10-20 15:00:05 -0500

    merged opening and tho df's

--- soybeans_xts.R
+++ soybeans_xts.R
@@ -16,13 +16,13 @@ breakout <- 2.00  #set the breakout distance to 2.00
 
 #convert .csv to xts
 df1 <- df %>%
-        select(-X)  #removes "X" col
+  select(-X)  #removes "X" col
 
 df1$datetime <- as.POSIXct(paste(df1$date, df1$time), format = "%m/%d/%Y %H:%M:%S")  #combines date and time cols
 
 df2 <- df1 %>%
-        select(datetime, price, volume = size) %>%  #removes individual data and time cols
-        arrange(datetime)  #arranges by datetime
+  select(datetime, price, volume = size) %>%  #removes individual data and time cols
+  arrange(datetime)  #arranges by datetime
 
 df_xts <- as.xts(df2[,-1], order.by = df2$datetime)  #convert to xts class
 df_xts <- make.index.unique(df_xts)  #makes each index unique; very important
@@ -34,8 +34,9 @@ df_xts3 <- lapply(df_xts2, cummax)
 df_xts4 <- do.call(rbind, df_xts3)
 names(df_xts4) <- c("daily_cummax_price", "daily_cummax_volume")
 df_xts5 <- merge(df_xts1, df_xts4)
-#
 
+
+#
 df_open <- df_xts["T09:30/T09:44"]  #subsets opening range interval; OPENINGS MINUTES ONLY DF
 df_open1 <- split(df_open, "days")
 df_open2 <- lapply(df_open1, cummax)
@@ -43,7 +44,16 @@ df_open3 <- do.call(rbind, df_open2)
 names(df_open3) <- c("opnRng_cummax_price", "opnRng_cummax_volume")
 df_open4 <- merge(df_open, df_open3)
 #
-df_main <- df_xts["T09:45/T14:14"]  #main part of soybeans trading day; MARKET HOURS ONLY NOT INCLUDING OPENING MINUTES
+df_xts5$orh_bo <- FALSE
+df_xts5$orh_bo <- ifelse((df_xts5$daily_cummax_price - breakout) > df_open4$opnRng_cummax_price, TRUE, FALSE)
+#merge df_open4 and df_xts5
+#assign all the missing values in df_xts5 the last value from df_open4$opnRng_cummax_price
+df_merged <- merge(df_open4, df_xts5)
+#find tidyquant or whatever package fun to select out unwanted/duplicate col's.
+#df_merged1 <- df_merged %>%
+#  select(-price.1, -volume.1)
+#
+#df_main <- df_xts["T09:45/T14:14"]  #main part of soybeans trading day; MARKET HOURS ONLY NOT INCLUDING OPENING MINUTES
 
 ##### ##### #####
 
@@ -53,7 +63,7 @@ orh_bo_index <- apply.daily(df_main, FUN = function(X) first(which(X %in% orh_bo
 orh_bo_matrix <- coredata(orh_bo_index)  #creates a matrix of the index breakout values
 
 #####
-                            
+
 ep_daily <- endpoints(df_main, on = "days")  #creates a daily index
 daily_last_index <- ep_daily[-1]  #removes the "0" value at the beginning
 daily_last_matrix <- t(t(daily_last_index))  #creates a matrix of last obs of day indexes
@@ -78,4 +88,4 @@ for(i in 2:ndays(df_main)) {
 #1: In which(in.index & all.indexes >= start) :
 #  Incompatible methods (">=.POSIXt", "Ops.xts") for ">="
 #2: In all.indexes >= start :
-#  longer object length is not a multiple of shorter object length                            
+#  longer object length is not a multiple of shorter object length 
